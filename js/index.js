8ad870acea9e0ce38feb8c6ea7e9182cee8f1c9f
dharrison37-gitit <308913916+dharrison37-gitit@users.noreply.github.com>
2026-09-07 01:56:49 -0500

    Create functions to make books and display them
    Created constructor function for Book as well as
    functions to add books to array and a function to
    display book data on webpage as cards.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 "use strict";
 
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -10,3 +13,44 @@ function Book(title, author, pages, read) {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${read ? "read it" : "not read yet"}`;
     };
 }
+
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
+function displayBooks(books) {
+    const container = document.querySelector(".container");
+    books.map((book) => {
+        let card = document.createElement("div");
+        card.className = "card";
+
+        let title = document.createElement("h2");
+        title.className = "card-title";
+        title.textContent = book.title;
+        card.appendChild(title);
+
+        let author = document.createElement("div");
+        author.className = "author";
+        author.textContent = `Author: ${book.author}`;
+        card.appendChild(author);
+
+        let pages = document.createElement("div");
+        pages.className = "pages";
+        pages.textContent = `Number of pages: ${book.pages}`;
+        card.appendChild(pages);
+
+        let read = document.createElement("div");
+        read.className = "read";
+        read.setAttribute("data-status", book.read);
+        read.textContent = `Read it? ${book.read ? "yes" : "not yet"}`;
+        card.appendChild(read);
+
+        container.appendChild(card);
+    });
+}
+
+addBookToLibrary("The Reason for God", "Tim Keller", 507, true);
+addBookToLibrary("Fairy Tales", "J.B. Jones", 200, false);
+
+displayBooks(myLibrary);
