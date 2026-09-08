e095d49eae44aacdccacdeade3d8ae7e5e2d993e
dharrison37-gitit <308913916+dharrison37-gitit@users.noreply.github.com>
2026-09-08 01:22:01 -0500

    Complete required functionality
    Everything is currently working.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const myLibrary = [];
+displayBooks(myLibrary);
 
 function Book(title, author, pages, read) {
     this.id = crypto.randomUUID();
@@ -9,10 +10,14 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
 
-    this.info = function () {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${read ? "read it" : "not read yet"}`;
-    };
+    // this.info = function () {
+    //     return `${this.title} by ${this.author}, ${this.pages} pages, ${read ? "read it" : "not read yet"}`;
+    // };
 }
+
+Book.prototype.toggleRead = function () {
+    this.read = !this.read;
+};
 
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
@@ -20,9 +25,11 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 function displayBooks(books) {
-    const container = document.querySelector(".container");
+    const container = document.querySelector(".grid-container");
+    container.textContent = "";
     books.map((book) => {
         let card = document.createElement("div");
+        card.setAttribute("data-id", book.id);
         card.className = "card";
 
         let title = document.createElement("h2");
@@ -40,19 +47,80 @@ function displayBooks(books) {
         pages.textContent = `Number of pages: ${book.pages}`;
         card.appendChild(pages);
 
-        let read = document.createElement("div");
+        let read = document.createElement("button");
         read.className = "read";
         read.setAttribute("data-status", book.read);
-        read.textContent = `Read it? ${book.read ? "yes" : "not yet"}`;
+        read.textContent = `${book.read ? "Read : Yes" : "Read : No"}`;
+        read.addEventListener("click", (e) => {
+            book.toggleRead();
+            displayBooks(myLibrary);
+        });
         card.appendChild(read);
+
+        let deleteButton = document.createElement("button");
+        deleteButton.className = "delete";
+        deleteButton.textContent = "Delete Book";
+        card.appendChild(deleteButton);
+        deleteButton.addEventListener("click", (e) => {
+            const indexToRemove = myLibrary.findIndex(
+                (obj) => obj.id === e.target.parentNode.dataset.id,
+            );
+
+            myLibrary.splice(indexToRemove, 1);
+            displayBooks(myLibrary);
+        });
 
         container.appendChild(card);
     });
 }
 
-addBookToLibrary("The Reason for God", "Tim Keller", 507, true);
-addBookToLibrary("Fairy Tales", "J.B. Jones", 200, false);
-addBookToLibrary("The Reason for God", "Tim Keller", 507, true);
-addBookToLibrary("Fairy Tales", "J.B. Jones", 200, false);
+const newBook = document.querySelector("#new-book");
+const addBook = document.querySelector("#add-book");
+const cancel = document.querySelector("#cancel");
+const dialog = document.querySelector("#my-dialog");
 
-displayBooks(myLibrary);
+newBook.addEventListener("click", (e) => {
+    dialog.showModal();
+});
+
+cancel.addEventListener("click", (e) => {
+    dialog.close();
+});
+
+addBook.addEventListener("click", (e) => {
+    e.preventDefault();
+    const form = document.querySelector("form");
+    const title = form.elements["title"].value;
+    const author = form.elements["author"].value;
+    const pages = form.elements["pages"].value;
+    const read = form.elements["read"].checked;
+
+    if (!title || !author) {
+        return;
+    } else {
+        addBookToLibrary(title, author, pages, read);
+        dialog.close();
+
+        displayBooks(myLibrary);
+
+        form.reset();
+    }
+});
+
+//#region Dummy Data for display
+
+function addDummyData() {
+    addBookToLibrary(
+        "Wherever you go, there you are",
+        "Jon Kabat-Zinn PhD",
+        309,
+        false,
+    );
+    addBookToLibrary("Think Like a Programmer", "V. Anton Spraul", 256, true);
+    addBookToLibrary("The Four Loves", "C. S. Lewis", 192, false);
+    addBookToLibrary("The Reason for God", "Tim Keller", 251, true);
+
+    displayBooks(myLibrary);
+}
+
+addDummyData();
