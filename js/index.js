6cd8fb8c11ac27fa546b5cd6df2c38b74a3f1cc4
dharrison37-gitit <308913916+dharrison37-gitit@users.noreply.github.com>
2026-09-15 20:21:39 -0500

    Replace constructor function with class
    Replaced the regular constructor function with
    a class definition and the prototype function
    was moved into the class as a method.

--- js/index.js
+++ js/index.js
@@ -3,17 +3,19 @@
 const myLibrary = [];
 displayBooks(myLibrary);
 
-function Book(title, author, pages, read) {
-    this.id = crypto.randomUUID();
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+class Book {
+    constructor(title, author, pages, read) {
+        this.id = crypto.randomUUID();
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-Book.prototype.toggleRead = function () {
-    this.read = !this.read;
-};
+    toggleRead() {
+        this.read = !this.read;
+    }
+}
 
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
